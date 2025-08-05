648d7db318f44bbc8893a5c3009c159b7405661f
shasin549 <shasinsha7384@gmail.com>
2025-08-05 22:09:54 +0530

    Update and rename vite.config.js to tailwind.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/betaauc/", // Base path for GitHub Pages deployment
-});
